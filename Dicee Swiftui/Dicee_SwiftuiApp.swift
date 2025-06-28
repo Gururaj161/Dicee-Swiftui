ac9bc6777f249c95889e9b03f53f0e9ed928c682
Gururaja M <gururajkumar161@gmail.com>
2025-06-28 22:46:59 +0530

    Initial Commit

---
+++ Dicee Swiftui/Dicee_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftuiApp.swift
+//  Dicee Swiftui
+//
+//  Created by Gururaja M on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
